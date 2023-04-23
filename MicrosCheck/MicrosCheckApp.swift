dac0c5ddcc799da62499c9ec808207c6d2af20e5
Egor Merkushev <egor.merkushev@corp.vk.com>
2023-04-23 16:28:30 +0300

    Initial Commit

---
+++ MicrosCheck/MicrosCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicrosCheckApp.swift
+//  MicrosCheck
+//
+//  Created by Egor Merkushev on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MicrosCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
