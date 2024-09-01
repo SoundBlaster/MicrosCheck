e871ac28da27068f183858751c627e68fc7cdc04
Egor Merkushev <712020:63dd2b60-3145-417b-9507-530999c6804b@bots.bitbucket.org>
2024-09-01 13:27:52 +0300

    Fix tests for the RecorderImpl.StateHolder

--- MicrosCheckTests/RecorderStateTests.swift
+++ MicrosCheckTests/RecorderStateTests.swift
@@ -11,11 +11,11 @@ import XCTest
 
 final class RecorderStateTests: XCTestCase {
 
-    var recorderState: Recorder.StateHolder?
+    var recorderState: RecorderImpl.StateHolder?
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        recorderState = Recorder.StateHolder()
+        recorderState = RecorderImpl.StateHolder()
     }
 
     override func tearDownWithError() throws {
